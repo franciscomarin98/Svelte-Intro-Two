77b769d772156f0b32693798a917a7eabc152ee7
francisco-1998 <francmarinc@gmail.com>
2021-03-30 10:52:45 -0500

    Probando si aprendi bien, y para que; pase el test basisco :v

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Counter from "./components/Counter.svelte";
+</script>
+
+<div class="App">
+	<h1>Contadores</h1>
+	<div class="contadores">
+		<Counter initialState={5} />
+		<Counter initialState={15} />
+		<Counter initialState={30} />
+	</div>
+</div>
+
+<style>
+	.contadores {
+		display: flex;
+	}
+</style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let initialState = 0;
+    let contador = initialState;
+
+    const handleClick = () => {
+        contador++;
+    };
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<div class="Counter">
+    <button on:click={handleClick}>
+        Contador: {contador}
+    </button>
+</div>
+
+<style>
+    .Counter {
+        margin: 10px;
+    }
+</style>
